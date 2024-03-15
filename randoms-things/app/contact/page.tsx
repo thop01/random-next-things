704665084fd67416fe1972d0c11092f88e339390
thop01 <p.thong@rocmondriaan.nl>
2024-03-15 14:53:53 +0100

    les 1

---
+++ randoms-things/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function contactPage() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>contact</h1>
+      </main>
+    );
+  }
