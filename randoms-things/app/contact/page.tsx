7faf0e3e83e8e930943e367ef3e76fbe245f7be0
thop01 <p.thong@rocmondriaan.nl>
2024-03-25 08:55:04 +0100

    uses states

--- randoms-things/app/contact/page.tsx
+++ randoms-things/app/contact/page.tsx
@@ -1,7 +1,17 @@
+"use client"
+import { useState } from "react";
+
 export default function contactPage() {
-    return (
+   
+  const [msgs, setMsgs] = useState([
+    {sender: "P. Thong", msg: "Hello world!" },
+    {sender: "P. Thong", msg: "Hello world!" },
+  ]);
+
+  return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>contact</h1>
+        <h1>{msgs[1].sender}</h1>
       </main>
     );
   }
